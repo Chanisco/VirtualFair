dff1b247d9415679e487bb120ca83b7c5f1b58a7
Chanisco <chaniscotromp@chaniscos-mbp.media.local>
2015-01-28 11:01:15 +0100

    GameScene added

--- Assets/Scripts/Track.cs
+++ Assets/Scripts/Track.cs
@@ -4,6 +4,6 @@ using System.Collections;
 public class Track : MonoBehaviour {
 
 	void Update () {
-		transform.Translate (Vector3.forward * Time.deltaTime * 10);
+		transform.Translate (Vector3.back * Time.deltaTime * 10);
 	}
 }
