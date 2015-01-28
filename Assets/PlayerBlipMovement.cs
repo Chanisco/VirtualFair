9db208fde903225cd059a91164542ac8e7cc232d
Jeffreeyy <17844@ma-web.nl>
2015-01-28 12:22:54 +0100

    GUI Player map movement

---
+++ Assets/PlayerBlipMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBlipMovement : MonoBehaviour {
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(transform.localPosition.y < 60)
+		{
+			transform.Translate (Vector3.up * 0.04545454545f);
+		}
+	}
+}
